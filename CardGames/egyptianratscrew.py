10feae627c9118d16cabd7c8a73339f66d5fd450
hfernan <hfernan@ncsu.edu>
2018-02-20 17:28:18 -0500

    added a deal method to Deck

--- CardGames/egyptianratscrew.py
+++ CardGames/egyptianratscrew.py
@@ -2,21 +2,6 @@
 
 import sys, traceback, time, random
 
-class Deck:
-    def __repr__(self):
-        return self.__class__.__name__
-        
-    def __init__(self, jokers=False):
-        suits = ["hearts","diamonds","spades","clubs"]
-        ranks = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']
-        self.cards = [Card(suit, rank) for suit in suits for rank in ranks]
-        if jokers:
-            self.cards.append(Card(None, "joker"))
-            self.cards.append(Card(None, "joker"))
-            
-    def shuffle(self):
-        return
-
 class Card:
     def __repr__(self):
         return self.__class__.__name__
@@ -31,6 +16,32 @@ class Card:
     def getRank(self):
         return self.rank
         
+    def isJoker(self):
+        if self.rank == "joker":
+            return true
+        return false
+
+class Deck:
+    def __repr__(self):
+        return self.__class__.__name__
+        
+    def __init__(self, jokers=False):
+        suits = ["hearts","diamonds","spades","clubs"]
+        ranks = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']
+        self.cards = [Card(suit, rank) for suit in suits for rank in ranks]
+        if jokers:
+            self.cards.append(Card(None, "joker"))
+            self.cards.append(Card(None, "joker"))
+            
+    def shuffle(self):
+        random.shuffle(self.cards)
+        
+    def deal(self, handSize):
+        self.shuffle()
+        for i in range(handSize):
+            yield self.cards[i]
+
+
 class Hand:
     def __repr__(self):
         return self.__class__.__name__
@@ -39,6 +50,6 @@ class Game:
     def __repr__(self):
         return self.__class__.__name__
         
-    def deal(self):
-        return
+class EgyptianRatscrew(Game):
+    return
         
