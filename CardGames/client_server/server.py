85950025b2af4f5eaad882476368bc46c6b59b78
Vincent Renich <vincent-github@renich.org>
2018-04-14 01:25:01 -0400

    Simplify server's processMsg and change it to support game-server queue communication
    Contributes to #16, #19, #29

--- CardGames/client_server/server.py
+++ CardGames/client_server/server.py
@@ -1,6 +1,9 @@
 import argparse
 import socket, threading
 from nettools import *
+import sys
+sys.path.insert(1,'../utils')
+import queue
 from EgyptianRatsCrew import EgyptianRatsCrew
 
 ADMIN = 'Server'
@@ -14,11 +17,21 @@ ERSNAME = ['ERS', 'EGYPTIANRATSCREW']
 LASTONENAME = ['LO', 'LASTONE']
 
 listener = None
-controller = None
+clientGetter = None
+clientSender = None
+gameThread = None
 gameMaster = None
 gmLock = threading.Lock()
 clientThreads = []
 game = None
+# The queue of messages for the game to handle
+gameQueue = queue.Queue()
+# The queue of messages for the server to handle
+responseQueue = queue.Queue()
+# Event for when the server puts a message in the queue for the game to handle
+serverEvent = threading.Event()
+# Event for when the game puts a message in the queue for the server to handle
+responseEvent = threading.Event()
 
 def cmdline():
     parser = argparse.ArgumentParser(description='Game server')
@@ -37,7 +50,7 @@ class Client(CommThread):
             if self.nameset == False:
                 if self.setName(msg):
                     print('{} has name {}'.format(self.addr, self.playerName))
-                    msgQueue.enqueue((controller, 'CONNECT ' + self.playerName))
+                    msgQueue.enqueue((clientGetter, 'CONNECT ' + self.playerName))
                     msgEvent.set()
                 else:
                     self.send('Invalid player name. Enter a new one')
@@ -49,7 +62,7 @@ class Client(CommThread):
         self.sock.close()
         print('{} ({}) has disconnected'.format(self.addr, self.playerName))
         if running:
-            msgQueue.enqueue((controller, 'DISCONNECT ' + self.playerName))
+            msgQueue.enqueue((clientGetter, 'DISCONNECT ' + self.playerName))
             msgEvent.set()
         clientThreads.remove(self)
         with gmLock:
@@ -83,70 +96,89 @@ def setGameMaster(client):
     gameMaster = client
     if client != None:
         print('{} is now the Game Master'.format(client.playerName))
-        msgQueue.enqueue((controller, 'MASTER ' + client.playerName))
+        msgQueue.enqueue((clientGetter, 'MASTER ' + client.playerName))
         msgEvent.set()
     else:
         print('Game Master is no longer set')
 
-def processMsg(name, msg):
-    global running
+def handleAdminMsg(msg):
+    tokens = msg.split(' ')
+    if tokens[0] == 'CONNECT':
+        otherPlayers = [c.playerName for c in clientThreads if c.playerName != tokens[1]]
+        responseQueue.enqueue((otherPlayers, '{}: {} has connected'.format(ADMIN, tokens[1])),
+                ([tokens[1]], ADMIN + ': ' + (('Current players are: ' + ', '.join(otherPlayers)) if len(otherPlayers) else 'No other players')))
+    elif tokens[0] == 'DISCONNECT':
+        otherPlayers = [c.playerName for c in clientThreads if c.playerName != tokens[1]]
+        responseQueue.enqueue((otherPlayers, '{}: {} has disconnected'.format(ADMIN, tokens[1])))
+    elif tokens[0] == 'MASTER':
+        allPlayers = [c.playerName for c in clientThreads]
+        responseQueue.enqueue((allPlayers, '{}: {} is now the Game Master'.format(ADMIN, tokens[1])))
+    else:
+        print('unknown ' + ADMIN + ' msg: ' + msg)
+
+def handleGMCmdMsg(msg):
     global game
-    if name == ADMIN:
-        tokens = msg.split(' ')
-        if tokens[0] == 'CONNECT':
-            otherPlayers = [c.playerName for c in clientThreads if c.playerName != tokens[1]]
-            return [(otherPlayers, '{}: {} has connected'.format(ADMIN, tokens[1])),
-                    ([tokens[1]], ADMIN + ': ' + (('Current players are: ' + ', '.join(otherPlayers)) if len(otherPlayers) else 'No other players'))]
-        elif tokens[0] == 'DISCONNECT':
-            otherPlayers = [c.playerName for c in clientThreads if c.playerName != tokens[1]]
-            return [(otherPlayers, '{}: {} has disconnected'.format(ADMIN, tokens[1]))]
-        elif tokens[0] == 'MASTER':
-            allPlayers = [c.playerName for c in clientThreads]
-            return [(allPlayers, '{}: {} is now the Game Master'.format(ADMIN, tokens[1]))]
-        else:
-            print('unknown ' + ADMIN + ' msg: ' + msg)
-        return []
+    global running
+    if msgArgs[0].upper() == CMDSTART:
+        if len(msgArgs) <= 1:
+            responseQueue.enqueue(([playerName], 'Invalid command'))
+        elif msgArgs[1].upper() in ERSNAME:
+            listener.close()
+            game = EgyptianRatsCrew([c.playerName for c in clientThreads], (gameQueue, serverEvent), (responseQueue, responseEvent))
+            responseQueue.enqueue(([c.playerName for c in clientThreads], 'Starting Egyptian Rat Screw.'))
+            gameThread = threading.Thread(target=game.run())
+            gameThread.start()
+        elif msgArgs[1].upper() in LASTONENAME:
+            responseQueue.enqueue(([c.playerName for c in clientThreads], 'Last One not server compatible.'))
+    elif msgArgs[0].upper() == CMDHALT:
+        running = False
 
+def handleCmdMsg(name, msg):
+    msgArgs = msg.split(' ')
+    if name == gameMaster.playerName:
+        handleGMCmdMsg(msg)
+    else:
+        responseQueue.enqueue(([name], 'Invalid command'))
+
+def processMsg(name, msg):
     if len(msg) == 0:
-        return []
-    if msg[0] == CMDLEADER:
-        msgArgs = msg.split(' ')
-        if msgArgs[0].upper() == CMDSTART and name == gameMaster.playerName:
-            if len(msgArgs) <= 1:
-                return [([name], 'Invalid command')]
-            if msgArgs[1].upper() in ERSNAME:
-                listener.close()
-                game = EgyptianRatsCrew([c.playerName for c in clientThreads])
-                return [([c.playerName for c in clientThreads], 'Starting Egyptian Rat Screw.')]
-            elif msgArgs[1].upper() in LASTONENAME:
-                return [([c.playerName for c in clientThreads], 'Last One not server compatible.')]
-        if msgArgs[0].upper() == CMDHALT and name == gameMaster.playerName:
-            running = False
-            return []
-        return [([name], 'Invalid command')]
-    if game and game.name == 'Egyptian Rats Crew':
-        return game.play()
-        return game.processMsg(name, msg)
-    return [([c.playerName for c in clientThreads if c.playerName != name], name + ': ' + msg)]
+        return
+    elif name == ADMIN:
+        handleAdminMsg(msg)
+    elif msg[0] == CMDLEADER:
+        handleCmdMsg(name, msg)
+    elif game and game.name == 'Egyptian Rats Crew':
+        gameQueue.enqueue((name, msg))
+    else:
+        responseQueue.enqueue(([c.playerName for c in clientThreads if c.playerName != name], name + ': ' + msg))
 
-def controllerMain():
+def clientSenderMain():
+    while running:
+        responseEvent.wait()
+        responseEvent.clear()
+        while responseQueue.notEmpty():
+            response = responseQueue.dequeue()
+            print(response[1])
+            for c in clientThreads:
+                if c.playerName in response[0]:
+                    c.send(response[1])
+
+def clientGetterMain():
     while running:
         msgEvent.wait()
         msgEvent.clear()
         while msgQueue.notEmpty():
             msg = msgQueue.dequeue()
-            responseList = processMsg(msg[0].playerName, msg[1])
-            for r in responseList:
-                print(r[1])
-                for c in clientThreads:
-                    if c.playerName in r[0]:
-                        c.send(r[1])
+            processMsg(msg[0].playerName, msg[1])
 
 if __name__ == "__main__":
     args = cmdline()
-    controller = threading.Thread(target=controllerMain)
-    controller.playerName = ADMIN
-    controller.start()
+    clientGetter = threading.Thread(target=clientGetterMain)
+    clientGetter.playerName = ADMIN
+    clientGetter.start()
+    clientSender = threading.Thread(target=clientSenderMain)
+    clientSender.playerName = ADMIN
+    clientSender.start()
     listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listener.bind((socket.gethostname(), args.port))
     print('Hostname: {}'.format(socket.gethostname()))
@@ -170,7 +202,9 @@ if __name__ == "__main__":
             if sock:
                 sock.close()
             break
-    controller.join()
+    clientGetter.join()
+    clientSender.join()
+    gameThread.join()
     print('Shutting down')
     for t in clientThreads:
         t.exit()
